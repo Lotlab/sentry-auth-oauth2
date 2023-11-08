9e8fb6fb54a217826a7f4e777bcc45f84f17a418
Jim Kirisame <jim@lotlab.org>
2023-11-08 21:26:52 +0800

    Update constants.py

--- sentry_auth_oauth2/constants.py
+++ sentry_auth_oauth2/constants.py
@@ -6,10 +6,10 @@ CLIENT_SECRET = getattr(settings, 'OAUTH2_API_SECRET', None)
 
 SCOPE = getattr(settings, 'OAUTH2_SCOPE', 'user:email')
 
-API_DOMAIN = getattr(settings, 'OAUTH2_API_DOMAIN','oauth2.com/api')
-
 BASE_DOMAIN = getattr(settings, 'OAUTH2_BASE_DOMAIN', 'oauth2.com')
 
 ACCESS_TOKEN_URL = getattr(settings, 'OAUTH2_ACCESS_TOKEN_URL', 'https://{0}/oauth/v2/token'.format(BASE_DOMAIN))
 
 AUTHORIZE_URL = getattr(settings, 'OAUTH2_AUTHORIZE_URL', 'https://{0}/oauth/v2/auth'.format(BASE_DOMAIN))
+
+USER_INFO_URL = getattr(settings, 'OAUTH2_USER_INFO_URL', 'https://{0}/user/me'.format(BASE_DOMAIN))
