bb5df97d573df9f5e05f90bfda8e6872b46f25be
Jim Kirisame <jim@lotlab.org>
2023-11-08 10:58:35 +0800

    Read access_token and authorize url from conf

--- sentry_auth_oauth2/constants.py
+++ sentry_auth_oauth2/constants.py
@@ -10,6 +10,6 @@ API_DOMAIN = getattr(settings, 'OAUTH2_API_DOMAIN','oauth2.com/api')
 
 BASE_DOMAIN = getattr(settings, 'OAUTH2_BASE_DOMAIN', 'oauth2.com')
 
-ACCESS_TOKEN_URL = 'https://{0}/oauth/v2/token'.format(BASE_DOMAIN)
+ACCESS_TOKEN_URL = getattr(settings, 'OAUTH2_ACCESS_TOKEN_URL', 'https://{0}/oauth/v2/token'.format(BASE_DOMAIN))
 
-AUTHORIZE_URL = 'https://{0}/oauth/v2/auth'.format(BASE_DOMAIN)
+AUTHORIZE_URL = getattr(settings, 'OAUTH2_AUTHORIZE_URL', 'https://{0}/oauth/v2/auth'.format(BASE_DOMAIN))
